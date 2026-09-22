c631b00a068e618e026d63503c9bd780d542d501
Alex Peirano <alexpeirano29@gmail.com>
2026-09-22 20:56:58 +0200

    func to remove folders from the public dir

--- src/main.py
+++ src/main.py
@@ -1,7 +1,25 @@
+import os
+import shutil
 from textnode import *
+import shutil
+
+public_path = "/Users/alexpeirano/Documents/personal coding/StaticSiteGenerator/MDtoHTML/public"
+
+static_path = "/Users/alexpeirano/Documents/personal coding/StaticSiteGenerator/MDtoHTML/static"
+
+def static_to_public(public_path:str, static_path: str):
+    for filename in os.listdir(public_path):
+        file_path = os.path.join(public_path, filename)
+
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+        else:
+            shutil.rmtree(file_path)
 
 def main():
-    node = TextNode('This is some anchor text', "link", 'https://www.boot.dev')
-    print(node)
-   
-main() 
+    static_to_public(public_path, static_path) 
+
+main()
+    
+
+    
